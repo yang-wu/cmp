f99b464a0f0d238a04b87c98267073346dfeb51a
yangwu <yangwu@tyun.cn>
2018-12-07 15:28:36 +0800

    修改错误的import

--- conf/default.py
+++ conf/default.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'app_control',
     'account',
     'home_application',
+    'home_application.cmpmanager',
 )
 
 # ==============================================================================
